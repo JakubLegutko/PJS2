e1d96cfb4bdb449ca9e26e480b4f6409f0bc7002
JakubLegutko <spraite111@gmail.com>
2023-01-25 21:37:24 +0100

    Reworked chatbot a bit

--- actions/actions.py
+++ actions/actions.py
@@ -26,7 +26,6 @@ def CheckOpen() -> bool:
         hours = json.load(f)
         current_time = datetime.datetime.now()
         today = list(hours['items'].keys())[current_time.weekday()]
-        #return current_time.hour > hours['items'][today]['open'] and current_time.hour < hours['items'][today]['close']
         return True
 
         
@@ -43,7 +42,7 @@ class ActionGetMenu(Action):
             for i in menu['items']:
                 dispatcher.utter_message(text="{} for the price of {} dollars and a prep time of {} hours".format(i['name'],i['price'],i['preparation_time']))
             return []
-        #dispatcher.utter_message(text=json.dumps(menu['items'], indent=4))
+
 
 
 
@@ -62,7 +61,7 @@ class ActionGetOpeningHours(Action):
         for i in hours['items']:
             dispatcher.utter_message(text="{} Open : {}:00".format(i,hours['items'][i]['open']))
             dispatcher.utter_message(text="Close : {}:00".format(hours['items'][i]['close']))
-        #dispatcher.utter_message(text=json.dumps(hours['items'], indent=4))
+
 
         return []
 
@@ -81,7 +80,7 @@ class ActionGetOpeningOnDay(Action):
         for i in hours['items']:
             dispatcher.utter_message(text="{} Open : {}:00".format(i,hours['items'][i]['open']))
             dispatcher.utter_message(text="Close : {}:00".format(hours['items'][i]['close']))
-        #dispatcher.utter_message(text=json.dumps(hours['items'], indent=4))
+
 
         return []
 
@@ -93,7 +92,7 @@ class ActionOrderDish(Action):
         tracker: Tracker, 
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         if not CheckOpen():
-            dispatcher.utter_message(text="Sorry, currently we are closed. Our Opening hours are:")
+            dispatcher.utter_message(text="Sorry, the restaurant is closed, try ordering between the following hours:")
             with open('data/opening_hours.json', 'r') as f:
                 hours = json.load(f)
             for i in hours['items']:
@@ -109,13 +108,12 @@ class ActionOrderDish(Action):
             dispatcher.utter_message(text="This item is not in our menu, please pick something from the menu")
             return []
         
-        dish_found = False
+        
         if order:
             for item in order:
                 for dish in menu["items"]:
                     if(fuzz.ratio(dish["name"].lower(), item.lower()) > 85):
-                        current_receipt.append(dish)
-                        dish_found = True     
+                        current_receipt.append(dish)   
             output_message.extend(order)
             
         if order_extra:
@@ -126,19 +124,19 @@ class ActionOrderDish(Action):
                             **dish,
                             "notes": item 
                         }
-                        current_receipt.append(dish_extra) 
-                        dish_found = True        
+                        current_receipt.append(dish_extra)      
             output_message.extend(order_extra)
         
-        if not dish_found:
-            dispatcher.utter_message(text="I did not find one of your order dishes in our menu. Please, try ordering again.")
+        if not len(current_receipt):
+            dispatcher.utter_message(text="One of the items you mentioned doesn't match any menu entries. Please, try ordering again.")
             return [SlotSet("order", []), SlotSet("order_extra",[])]
 
-        if len(output_message) == 0 :
-            dispatcher.utter_message(text="I did not find one of your order dishes in our menu. Please, try ordering again.")
+        if not len(output_message) :
+            dispatcher.utter_message(text="One of the items you mentioned doesn't match any menu entries. Please, try ordering again.")
             return [SlotSet("order", []), SlotSet("order_extra",[])]
 
-        dispatcher.utter_message(text="I have added {} to your receipt sir/mam. Anything else?".format(output_message))
+        dispatcher.utter_message(text="I have added {} to your order. Anything else?".format(output_message))
+        dispatcher.utter_message("If any of the ordered items do not match your desired order, please express the intent to clear the order and try again")
         return [SlotSet("order", []), SlotSet("order_extra",[])]
 
 class ActionConfirmOrder(Action):
@@ -152,7 +150,7 @@ class ActionConfirmOrder(Action):
         total_names = []
         for dish in current_receipt:
             total_names.append(dish["name"])
-        dispatcher.utter_message(text="Can we confirm that this is everything you ordered? {}".format(total_names))
+        dispatcher.utter_message(text="Could you please confirm the order? {}".format(total_names))
         return []
 
 class ActionClearOrder(Action):
@@ -176,12 +174,12 @@ class ActionSumUpTheOrder(Action):
         dispatcher: CollectingDispatcher, 
         tracker: Tracker, 
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        total_time = 0
-        total_spend = 0
+        time = 0
+        money = 0
         for dish in current_receipt:
-            total_spend+=dish["price"]
-            total_time+=dish["preparation_time"]
+            money+=dish["price"]
+            time+=dish["preparation_time"]
 
-        dispatcher.utter_message(text="Thank You for ordering in our restaurant. We inform You that your shipment will be ready for transport in {} minutes. The total cost is {}$. Have a nice meal :-)".format(total_time*60, total_spend))
+        dispatcher.utter_message(text="Thank You for ordering in our restaurant. We inform You that your shipment will be ready for transport in {} minutes. The total cost is {}$. Have a nice meal :-)".format(time*60, money))
         current_receipt.clear()
         return []
